a830e97a5510831a3798d54ba47da19a11a9d31f
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2025-11-28 04:52:47 -0400

    feat: add Hero tablet view

--- src/features/maintenance/Hero.vue
+++ src/features/maintenance/Hero.vue
@@ -14,29 +14,37 @@ import BaseButton from '@/components/ui/BaseButton.vue';
     />
 
     <div
-      class="flex absolute left-20 top-5 max-w-[514px] rounded-sm bg-amBluePremium text-white shadow-2xl"
+      class="flex absolute md:left-[62px] lg:left-20 top-5 md:max-w-[378px] lg:max-w-[514px] rounded-sm bg-amBluePremium text-white shadow-2xl"
     >
-      <div class="pl-[34px] py-5">
-        <h2 class="text-sm font-GarnettSemibold font-semibold">
+      <div class="pl-[34px] py-5 md:pr-2 lg:pr-0">
+        <h2 class="md:text-lg lg:text-sm font-GarnettSemibold font-semibold">
           Servicio no disponible
         </h2>
 
         <h3
-          class="mt-5 text-4xl font-GarnettSemibold font-semibold leading-tight"
+          class="mt-5 md:text-[28px] lg:text-4xl font-GarnettSemibold font-semibold leading-tight"
         >
           Sitio en mantenimiento
         </h3>
 
-        <p class="mt-5 text-base">
+        <p class="md:mt-[15px] lg:mt-5 text-base">
           Te sugerimos llamar a Global Support o escribirnos por el chat.
         </p>
 
-        <BaseButton class="mt-5" variant="primary" size="lg">
+        <BaseButton
+          class="hidden md:hidden lg:block lg:mt-5"
+          variant="primary"
+          size="lg"
+        >
+          Chat Aeroméxico
+        </BaseButton>
+
+        <BaseButton class="hidden lg:hidden mt-[15px] py-3" variant="primary">
           Chat Aeroméxico
         </BaseButton>
       </div>
 
-      <img :src="stelasIcon" alt="estelas" />
+      <img :src="stelasIcon" alt="estelas" class="relative h-full" />
     </div>
   </section>
 </template>
