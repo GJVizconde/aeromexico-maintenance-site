cac32687ceb34ac83f2b5b501a1e9ee19c679ad2
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2025-11-28 16:45:40 -0400

    fix: modify mx in Support

--- src/features/maintenance/Support.vue
+++ src/features/maintenance/Support.vue
@@ -34,7 +34,7 @@ const scrollTablet = (direction: 'prev' | 'next') => {
   <section class="py-10 w-full">
     <div class="max-w-[1120px] mx-auto">
       <div
-        class="flex items-center justify-between gap-3 md:max-w-[645px] md:mx-auto lg:max-w-none"
+        class="flex items-center mx-5 justify-between gap-3 md:max-w-[645px] md:mx-auto lg:max-w-none"
       >
         <h2
           class="md:text-[22px] lg:text-[32px] font-GarnettSemibold font-semibold text-amBluePremium md:leading-[42px]"
