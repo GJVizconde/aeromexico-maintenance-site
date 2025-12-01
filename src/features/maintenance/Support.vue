37fdd80510ca7f28fd04eaea070f7e42b6c7e239
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2025-11-30 21:11:40 -0400

    feat: add Suppoort Card

--- src/features/maintenance/Support.vue
+++ src/features/maintenance/Support.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import type { SupportItem } from './maintenance.types';
-
-import stelasMobileIcon from '@/assets/icons/stelas-mobile.svg';
 import arrowIcon from '@/assets/icons/arrow.svg';
+import SupportCard from './SupportCard.vue';
+import type { SupportItem } from './maintenance.types';
 
 interface Props {
   items: SupportItem[];
@@ -70,45 +69,7 @@ const scrollTablet = (direction: 'prev' | 'next') => {
         ref="tabletSliderRef"
         class="mt-5 flex gap-5 overflow-x-auto scroll-smooth snap-x snap-mandatory w-full md:w-full md:max-w-none md:pl-[calc((100%-645px)/2)] md:pr-0 lg:grid lg:grid-cols-4 lg:gap-8 lg:overflow-visible lg:max-w-none lg:mx-0 lg:px-0"
       >
-        <article
-          v-for="(item, idx) in items"
-          :key="idx"
-          class="group relative shrink-0 overflow-hidden rounded-sm shadow-lg snap-start md:shrink-0 lg:min-w-0 lg:max-w-none"
-        >
-          <!-- Imagen -->
-          <img
-            :src="item.img"
-            alt=""
-            class="h-[382px] w-[256px] md:w-[259px] lg:w-full object-cover transition duration-500 group-hover:scale-105"
-          />
-
-          <!-- Overlay opcional -->
-          <div class="pointer-events-none absolute inset-0" />
-
-          <!-- Panel azul dentro de la foto -->
-          <div class="absolute left-[15px] right-[15px] bottom-[15px]">
-            <div
-              class="flex items-start justify-between max-w-[229px] lg:w-full rounded-sm bg-amBluePremium pl-4 py-[15px] shadow-lg"
-            >
-              <div>
-                <h3
-                  class="text-[22px] font-GarnettSemibold font-semibold text-white"
-                >
-                  {{ item.title }}
-                </h3>
-
-                <p class="mt-2.5 text-sm leading-5 text-white">
-                  {{ item.text }}
-                </p>
-              </div>
-
-              <div class="mt-1 w-auto shrink-0">
-                <img :src="stelasMobileIcon" alt="estelas" />
-                <img :src="stelasMobileIcon" class="mt-1" alt="estelas" />
-              </div>
-            </div>
-          </div>
-        </article>
+        <SupportCard v-for="(item, idx) in items" :key="idx" :item="item" />
       </div>
     </div>
   </section>
---
+++ src/features/maintenance/SupportCard.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import stelasMobileIcon from '@/assets/icons/stelas-mobile.svg';
+import type { SupportItem } from './maintenance.types';
+
+interface Props {
+  item: SupportItem;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <article
+    class="group relative shrink-0 overflow-hidden rounded-sm shadow-lg snap-start md:shrink-0 lg:min-w-0 lg:max-w-none"
+  >
+    <img
+      :src="props.item.img"
+      alt=""
+      class="h-[382px] w-[256px] md:w-[259px] lg:w-full object-cover transition duration-500 group-hover:scale-105"
+    />
+
+    <div class="pointer-events-none absolute inset-0" />
+
+    <div class="absolute left-[15px] right-[15px] bottom-[15px]">
+      <div
+        class="flex items-start justify-between max-w-[229px] lg:w-full rounded-sm bg-amBluePremium pl-4 py-[15px] shadow-lg"
+      >
+        <div>
+          <h3 class="text-[22px] font-GarnettSemibold font-semibold text-white">
+            {{ props.item.title }}
+          </h3>
+
+          <p class="mt-2.5 text-sm leading-5 text-white">
+            {{ props.item.text }}
+          </p>
+        </div>
+
+        <div class="mt-1 w-auto shrink-0">
+          <img :src="stelasMobileIcon" alt="estelas" />
+          <img :src="stelasMobileIcon" class="mt-1" alt="estelas" />
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
